6cf53272d0fda17afebe851b6ef95c5e3687ed54
Kris Gesling <krisgesling@gmail.com>
2019-06-20 11:56:58 +0930

    add regex

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler
+
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
 
 
 class Regex(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('regex.intent')
-    def handle_regex(self, message):
-        self.speak_dialog('regex')
+    @intent_handler(IntentBuilder("").require("test").require("alpha").optionally("Location"))
+    def handle_query_time(self, message):
+        print(message.data.keys())
+        location = message.data['Location']
+        print(location)
+        self.speak_dialog('regex', {'location': location})
 
 
 def create_skill():
     return Regex()
-
